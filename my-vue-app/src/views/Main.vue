acaef89476668b5cf2e2b583c4d38fa021c23d0f
Vawet <2874184050@qq.com>
2025-04-22 21:08:13 +0800

    my-vue-app/src/views/Main.vue

--- my-vue-app/src/views/Main.vue
+++ my-vue-app/src/views/Main.vue
@@ -11,6 +11,7 @@ import CommonHeader from '@/components/CommonHeader.vue'
                 <el-header>
                     <common-header />
                     <!-- 要实现 两个的联动 需要使用pinia -->
+                     <!-- 秒的fw -->
                 </el-header>
                 <el-main class="right-main">
                     main
