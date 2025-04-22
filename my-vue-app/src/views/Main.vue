11425500a16db7f1cb1c9a3f6dc9a0112a1cfc74
Vawet <2874184050@qq.com>
2025-04-22 21:00:26 +0800

    Main.vue

--- my-vue-app/src/views/Main.vue
+++ my-vue-app/src/views/Main.vue
@@ -1,6 +1,5 @@
 <script setup>
 import CommonAside from '@/components/CommonAside.vue'
-// 别名 @ 代替 src     把组件components文件夹中的CommonAside.vue文件引入
 import CommonHeader from '@/components/CommonHeader.vue'
 </script>
 
@@ -8,11 +7,9 @@ import CommonHeader from '@/components/CommonHeader.vue'
     <div class="common-layout">
         <el-container class="lay-container">
             <common-aside />
-            <!-- 这里就是CommonHeader组件 -->
             <el-container>
                 <el-header>
                     <common-header />
-                    <!-- 这里就是CommonAside组件 -->
                 </el-header>
                 <el-main class="right-main">
                     main
